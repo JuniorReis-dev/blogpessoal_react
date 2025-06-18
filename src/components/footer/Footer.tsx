7fe0b46c6e28c1c889d5e35b5b0168144714e4c6
JuniorReis-dev <cleiton.nr47@gmail.com>
2025-06-18 10:57:10 -0300

    :sparkles: Adicionado page home

---
+++ src/components/footer/Footer.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { LinkedinLogo, GithubLogo } from "@phosphor-icons/react";
+
+export default function Footer() {
+  let data = new Date().getFullYear();
+  return (
+    <div>
+      <div className="flex justify-center bg-indigo-900 text-white">
+        <div className="container flex flex-col items-center py-4">
+          <p className="text-xl font-bold">
+            Blog Pessoal Cleiton Reis | Copyright: {data}
+          </p>
+          <p className="text-lg">Acesse nossas redes sociais</p>
+          <div className="flex gap-2">
+            <LinkedinLogo size={48} weight="bold" />
+            <GithubLogo size={48} weight="bold" />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
