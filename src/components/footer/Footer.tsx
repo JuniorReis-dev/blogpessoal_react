1053fb42f0a1a9ab8c43b0f90c397eda011f7ddb
JuniorReis-dev <cleiton.nr47@gmail.com>
2025-06-23 09:47:18 -0300

    :spakles: Criando componentes login e cadastro

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -11,8 +11,15 @@ export default function Footer() {
           </p>
           <p className="text-lg">Acesse nossas redes sociais</p>
           <div className="flex gap-2">
-            <LinkedinLogo size={48} weight="bold" />
-            <GithubLogo size={48} weight="bold" />
+            <a
+              href="https://www.linkedin.com/in/cleitonreisbr/"
+              target="_blank"
+            >
+              <LinkedinLogo size={48} weight="bold" />
+            </a>
+            <a href="https://github.com/JuniorReis-dev" target="_blank">
+              <GithubLogo size={48} weight="bold" />
+            </a>
           </div>
         </div>
       </div>
--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom";
+
 export default function navbar() {
   return (
     <div
@@ -9,9 +11,29 @@ export default function navbar() {
         text-white"
     >
       <div className="container flex justify-between text-lg">
-        Blog Pessoal
+        <Link
+          to="/home"
+          className="text-2xl font-bold
+					"
+        >
+          Blog Pessoal
+        </Link>
         <div className="flex gap-4">
-          Postagens Temas Cadastrar tema Perfil Sair
+          <Link to="/postagens" className="hover:underline">
+            Postagens
+          </Link>
+          <Link to="/temas" className="hover:underline">
+            Temas
+          </Link>
+          <Link to="/cadastrartema" className="hover:underline">
+            Cadastrar tema
+          </Link>
+          <Link to="/perfil" className="hover:underline">
+            Perfil
+          </Link>
+          <Link to="" className="hover:underline">
+            Sair
+          </Link>
         </div>
       </div>
     </div>
